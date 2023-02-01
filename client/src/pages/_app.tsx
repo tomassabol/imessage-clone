2be81d3bb49d02d8deb77d18865048768f6fff5d
Tomáš Sabol <tomassabol2@gmail.com>
2023-02-01 14:57:02 +0100

    chakra ui setup

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../chakra/theme";
 
-function MyApp({ Component, pageProps: { session, ...pageProps} }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 }
