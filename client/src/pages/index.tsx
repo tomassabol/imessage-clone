4f37363c88157b8599255973bbcfe5629be6e4cc
Tomáš Sabol <tomassabol2@gmail.com>
2023-02-01 14:31:08 +0100

    create user

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import type { NextPage } from "next";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const Home: NextPage = () => {
+  const { data } = useSession();
+
+  console.log("Here is data:", data);
+
   return (
     <>
-    <div>Hello page</div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>Sign out</button>
+      ) : (
+        <button onClick={() => signIn("google")}>Sign in</button>
+      )}
+      {data?.user?.name}
     </>
-  )
-}
+  );
+};
+
+export default Home;
